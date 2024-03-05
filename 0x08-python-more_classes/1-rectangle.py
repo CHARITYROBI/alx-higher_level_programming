82375feb504c5f128a666dfb81567c0fb09d50f3
CHARITYROBI <charityrobi36@gmail.com>
2024-03-05 13:52:43 +0300

    Update

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,23 +1,42 @@
-#include <stdio.h>
-#include "lists.h"
+#!/usr/bin/python3
+"""Defines a Rectangle class."""
 
-/**
- * print_dlistint - Prints a doubly linkedlist of integers
- *
- * @h: A pointer to the first element of a list
- *
- * Return: The number of element printed
- */
-size_t print_dlistint(const dlistint_t *h)
-{
-	size_t n;
 
-	n = 0;
-	while (h)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		n++;
-	}
-	return (n);
-}
+class Rectangle:
+    """Represent a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get/set the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get/set the height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
